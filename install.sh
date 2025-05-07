4361d12fcc56bd5fd3098e0be5ccb4e7093cfd45
Lorenzo Ledda <115366197+diodoLedd@users.noreply.github.com>
2025-05-07 19:25:03 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+set -e
+
+DEST="/usr/local/bin"
+PINOPATH="$DEST/pino"
+
+echo "📦 Installation of Pino the Washing Machine in $DEST..."
+
+if [ ! -f "./pino" ]; then
+  echo "❌ Error: the file ‘pine’ was not found in the current directory."
+  exit 1
+fi
+
+sudo cp ./pino "$PINOPATH"
+sudo chmod +x "$PINOPATH"
+
+echo "✅ Installation completed!"
+echo "🚀 Now you can run 'pino' from any terminal."
